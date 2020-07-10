5aafd6a554d157a5144823c61c938cc73330aa84
myDeveloperJourney <daniel@mydeveloperjourney.com>
2020-07-10 18:42:28 -0500

    adds final version

--- js/script.js
+++ js/script.js
@@ -1,68 +1,85 @@
-/*----- constants -----*/
-const baseURL = 'https://pokeapi.co/api/v2/pokemon';
+// jQuery IIFE
+$(function() {
+    /*----- constants -----*/
 
-/*----- app's state (variables) -----*/
-let pokemon, pokemonDetail;
+    const baseURL = 'https://pokeapi.co/api/v2/pokemon';
 
-/*----- cached element references -----*/
+    /*----- app's state (variables) -----*/
+    let pokemon, pokemonDetail;
 
-const $ulEl = $('.collection');
-const $imgEl = $('.modal-content img');
-const $name = $('#name');
-const $moves = $('#moves');
-const $abilities = $('#abilities');
-const $height = $('#height');
-const $modal = $('.modal');
+    /*----- cached element references -----*/
 
-/*----- event listeners -----*/
-$ulEl.on('click', 'span', handleClick);
+    const $ulEl = $('.collection');
+    const $imgEl = $('.modal-content img');
+    const $name = $('#name');
+    const $moves = $('#moves');
+    const $abilities = $('#abilities');
+    const $height = $('#height');
+    const $modal = $('.modal');
 
-/*----- functions -----*/
+    /*----- event listeners -----*/
+    $ulEl.on('click', 'span', handleClick);
 
-// initialize modal
-$modal.modal();
-const instance = M.Modal.getInstance($modal);
+    /*----- functions -----*/
 
-function handleClick(event) {
-    getPokemon(event.target.dataset.url, true);
-}
+    // initialize modal
+    $modal.modal();
+    const instance = M.Modal.getInstance($modal);
 
-// make the data available as soon as the app loads
-getPokemon();
+    function handleClick(event) {
+        getPokemon(event.target.dataset.url, true);
+    }
 
-function getPokemon(detailURL, isDetail) {
-    
-    const url = detailURL || baseURL;
+    // make the data available as soon as the app loads
+    getPokemon();
 
-    $.ajax(url)
-    .then(
-       function(data) {
-           if(!isDetail) {
-               pokemon = data.results;
-               render(); // programatically render the html
-           } else {
-               pokemonDetail = data;
-           }
-    }, function(error){
-        console.log("Error: ", error);
-    });
-}
+    function getPokemon(detailURL, isDetail = false) {
+        
+        const url = detailURL || baseURL;
+
+        $.ajax(url)
+        .then(
+        function(data) {
+            if(!isDetail) {
+                pokemon = data.results;
+                render(); // programatically render the html
+            } else {
+                pokemonDetail = data;
+                render(true);
+            }
+        }, function(error){
+            console.log("Error: ", error);
+        });
+    }
 
 
-function generateHTML() {
-    return pokemon.map(function(p) {
-        return `
-        <li class="collection-item red-text">
-            <div style="text-transform: capitalize;">${p.name}
-                <span data-url="${p.url}" class="secondary-content blue-text">
-                Detail
-                </span>
-            </div>
-        </li>`;
-    });
-}
+    function generateHTML() {
+        return pokemon.map(function(p) {
+            return `
+            <li class="collection-item red-text">
+                <div style="text-transform: capitalize;">${p.name}
+                    <span data-url="${p.url}" class="secondary-content blue-text">
+                    Detail
+                    </span>
+                </div>
+            </li>`;
+        });
+    }
 
-function render() {
-    const html = generateHTML().join("");
-    $ulEl.html(html)
-}
+    function render(isDetail = false) {
+        if(!isDetail) {
+            const html = generateHTML().join("");
+            $ulEl.html(html)
+        } else {
+            // produce the modal
+            $imgEl.attr('src', pokemonDetail.sprites.front_default);
+            $imgEl.attr('alt', pokemonDetail.name);
+            $name.text(pokemonDetail.name);
+            $height.text("Height: " + pokemonDetail.height);
+            $moves.text("Number of moves: " + pokemonDetail.moves.length);
+            $abilities.text("Number of abilities: " + pokemonDetail.abilities.length);
+            // open the modal
+            instance.open();
+        }
+    }
+});
